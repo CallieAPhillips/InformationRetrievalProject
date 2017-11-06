a03cd51a67e3ec44b545650773a7ac9900363c42
CallieAPhillips <CallieAPhillips@virginia.edu>
2017-11-05 22:19:21 -0500

    decided having dates of tweets might be helpful for getting a diverse sample

--- TweetModule/src/Tweet.java
+++ TweetModule/src/Tweet.java
@@ -3,6 +3,7 @@ import twitter4j.Status;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 import static java.lang.System.exit;
 
@@ -14,14 +15,16 @@ public class Tweet {
     private long id;
     private String[] hashtags;
     private String csvFileName = "tweets.csv";
+    private Date time;
 
-    //Format of CSV file (currently) = category, user, content, hashtag[0], hashtag[1], ...
+    //Format of CSV file (currently) = category, user, time, content, hashtag[0], hashtag[1], ...
 
-    public Tweet(String content, String user, long id, String[] hashtags) {
+    public Tweet(String content, String user, long id, String[] hashtags, Date time) {
         this.content = content.replace("\n", " ").replace(",", "");
         this.user = user;
         this.id = id;
         this.hashtags = hashtags;
+        this.time = time;
         setCategory("no category yet");
         this.printToCSV();
     }
@@ -34,6 +37,7 @@ public class Tweet {
             this.content = status.getText().replace("\n", " ").replace(",", "");
             this.user = status.getUser().getScreenName().replace(",", "");
             this.id = status.getId();
+            this.time = status.getCreatedAt();
 
             hashtags = new String[hashtagEntities.length];
             for (int i = 0; i < hashtagEntities.length; i++) {
@@ -45,7 +49,7 @@ public class Tweet {
     }
 
     public void printToCSV() {
-        String line = String.format("%s,%s,%s", this.category, this.user, this.content);
+        String line = String.format("%s,%s,%s,%s", this.category, this.user, this.time.toString(), this.content);
         for(String tag: this.hashtags) line =line.concat(","+tag);
         line=line.concat("\n");
         System.out.println(line);
